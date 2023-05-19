f8296de5ed9157af326aee59a1b60346748b7bf9
unknown <ahmadirvan9c17@gmail.com>
2023-05-19 22:39:36 +0700

    adding Procfile and secure assets

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet"
         crossorigin="anonymous" />
-    <link href="css/app.css" rel="stylesheet">
+    <link href="{{ secure_asset('/css/app.css') }}" rel="stylesheet" />
     <title>@yield('title', 'Online Store')</title>
 </head>
 
